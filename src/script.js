9ddc310bd35b84898359a382d7b53854d5108423
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-06-27 19:17:42 -0400

    add-change-colors

--- src/script.js
+++ src/script.js
@@ -5,3 +5,15 @@ hamburger.addEventListener("click", () => {
 	const fix = document.querySelector(".fix")
 	fix.classList.toggle("move")
 })
+const header = document.querySelector("header")
+window.addEventListener("scroll", () => {
+	header.classList.toggle("sticky", window.scrollY > 0)
+	
+	if (window.scrollY > 0) {
+		document.querySelector("meta[name='theme-color']").setAttribute("content", "#ffa500")
+	}
+	else{
+		document.querySelector("meta[name='theme-color']").setAttribute("content", "#4c0079")
+	}
+
+})
